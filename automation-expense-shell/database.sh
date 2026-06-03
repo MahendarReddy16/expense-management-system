a8742b5b127b540bd647daf72004772669dd5cfa
Mahendar Reddy Tummalapally <mahendarreddy94151@gmail.com>
2026-06-03 14:31:02 +0530

    manual

--- automation-expense-shell/database.sh
+++ automation-expense-shell/database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Creating the log folder 
-LOG_FOLDER= "/var/log/shell-script"
+LOG_FOLDER= "/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
@@ -22,18 +22,18 @@ N="\e[0m"
 VALIDATE () {  
     if [ $1 -ne 0 ]
    then 
-      echo -e "$2 $R FAILURE $N"
+      echo -e "$2 $R FAILURE $N" | tee -e $LOG_FILE
       exit 1
    else
-      echo -e "$2 $G SUCCESS!..$N"
+      echo -e "$2 $G SUCCESS!..$N" | tee -e $LOG_FILE
    fi 
 }
 
-echo "Script started execution at $(date)"
+echo "Script started execution at $(date)" | tee -e $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-   echo -e " $R Root Previlegous required $N "
+   echo -e " $R Root Previlegous required $N " | tee -e $LOG_FILE
    exit 1
 fi
 
@@ -54,7 +54,7 @@ then
    systemctl enable mysqld
    VALIDATE $? "Enabled mysql Server"
 else
-   echo "  mysql-server installed, Nothing to do. "
+   echo -e " $Y mysql-server installed, Nothing to do. $N "
 fi
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
